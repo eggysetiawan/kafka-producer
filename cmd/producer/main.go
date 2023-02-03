16a3a2480b494bc9a31527b51ece702b6554f055
Rahmat Setiawan <setiawaneggy@gmail.com>
2023-02-03 09:50:34 +0700

    initial commit

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/Shopify/sarama"
 )
 
+type Body struct {
+	Message string      `json:"message"`
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+}
+
+func NewBody(message string) Body {
+	return Body{
+		Message: message,
+		Code:    http.StatusOK,
+		Data:    []string{},
+	}
+}
+
 var (
 	brokerList = kingpin.Flag("brokerList", "List of brokers to connect").Default("localhost:9092").Strings()
-	topic      = kingpin.Flag("topic", "Topic name").Default("important").String()
+	topic      = kingpin.Flag("topic", "Topic name").Default("EJLOG").String()
 	maxRetry   = kingpin.Flag("maxRetry", "Retry limit").Default("5").Int()
 )
 
@@ -29,13 +48,44 @@ func main() {
 			log.Panic(err)
 		}
 	}()
+
+	//b := NewBody("test")
+	//
+	//wm, err := json.Marshal(b)
+	//if err != nil {
+	//	return
+	//}
+
+	content := map[string]string{}
+
+	//content["key"] = "A"
+	//content["value"] = "DATA EJLOG A"
+
+	content["key"] = "B"
+	content["value"] = "DATA EJLOG B"
+
 	msg := &sarama.ProducerMessage{
 		Topic: *topic,
-		Value: sarama.StringEncoder("Something Cool"),
+		Key:   sarama.StringEncoder(content["key"]),
+		Value: sarama.StringEncoder(content["value"]),
 	}
+
 	partition, offset, err := producer.SendMessage(msg)
+
 	if err != nil {
 		log.Panic(err)
 	}
+
 	log.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", *topic, partition, offset)
+
+}
+
+func unixTime() string {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	n, _ := fmt.Print(r1.Intn(100), ",")
+
+	return strconv.Itoa(n)
+
 }
